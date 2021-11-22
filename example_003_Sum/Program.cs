369d0e7509e11a9afdf9d84dbef4b3aaaa6968d9
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-11-22 23:43:25 +0300

    edit example_003_sum

--- example_003_Sum/Program.cs
+++ example_003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 364732198;
-int numberB = 578312612;
+﻿int numberA = new Random () .Next(1, 10); // 1,2,3,4...9
+Console.WriteLine(numberA);
+int numberB = new Random () .Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
