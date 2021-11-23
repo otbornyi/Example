7b6af78acc88c854605e1c1e064ce704e042e9a1
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-11-23 21:35:19 +0300

    Add example 007 Base

---
+++ Example_007_Base/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 1;
+int b = 4;
+int c = 783265;
+int d = 2;
+int e = 6;
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write(max);
